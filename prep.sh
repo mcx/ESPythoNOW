2b7d86a6758ad1c57c6b082b798e5e60a01dee50
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2026-02-02 22:41:15 -0800

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
 
-iw dev $1 set type monitor
-iw dev $1 set channel $2
-ip link set $1 up
+echo "Warning: prep.sh is deprecated. Please use --channel 8 and --set_interface=True when calling ESPythoNOW."
+
+# Try method 1
+ifconfig $1 down && \
+iwconfig $1 mode monitor && \
+ifconfig $1 up && \
+iwconfig $1 channel $2
+
+# If method 1 failed, try method 2
+if [ $? -ne 0 ]; then
+  iw dev $1 set type monitor
+  iw dev $1 set channel $2
+  ip link set $1 up
+fi
