fa5e16a954d8ef7247c15ee88030fd47437bb9a6
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2026-02-02 23:22:24 -0800

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -11,6 +11,6 @@ iwconfig $1 channel $2
 # If method 1 failed, try method 2
 if [ $? -ne 0 ]; then
   iw dev $1 set type monitor
-  iw dev $1 set channel $2
   ip link set $1 up
+  iw dev $1 set channel $2
 fi
